393a65c67055bcef4a9d55a8266863bbf0a1aa0d
Lucas <joselucas_2004@hotmail.com>
2021-04-19 17:47:53 -0300

    Readme atualizado e arquivos js adicionados para validação de senha

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 # Teste de cadastro
+
+Para executar o código, após baixar, basta executar o arquivo .httml.
+A página de cadastro será exibida.
+
+Ao clicar no botão "Cadastrar", o usuário receberá uma mensagem de aviso caso algum dos campos não tenha sido preenchido.
+
+Ao selecionar a caixa "senha", uma nova imagem aparece, listando os requisitos para a validação da mesma, que, conforme a senha é digitada, os requisitos atingidos mudam de vermelho para verde.
--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-function senha() {
-    var password = document.getElementById('senha')
-    console.log(password)
-    if (password.length < 8) {
-        console.log("A senha deve ter entre 8 e 14 caracteres!")
-    }
-    if (password.length >= 8) {
-        if (password.length <= 14) {
-            console.log("Número de caracteres válidos!")
-        } else {
-            console.log("A senha deve ter entre 8 e 14 caracteres!")
-        }
-    }
-}
-function soma() {
-    console.log('qualquer coisa')
-}
+var senha = document.getElementById('pw');
+var minuscula = document.getElementById('minuscula');
+var maiuscula = document.getElementById('maiuscula');
+var numero = document.getElementById('numero');
+var tamanho = document.getElementById('tamanho');
+var caractere = document.getElementById('caractere');
 
+senha.onfocus = function () {
+    document.getElementById("mensagem").style.display = "block";
+}
+senha.onblur = function () {
+    document.getElementById("mensagem").style.display = "none";
+}
+senha.onkeyup = function () {
+
+    if (senha.value.length >= 8 && senha.value.length < 15) {
+        tamanho.classList.remove("invalido");
+        tamanho.classList.add("valido");
+    } else {
+        tamanho.classList.remove("valido");
+        tamanho.classList.add("invalido");
+    }
+
+    var numeral = /[0-9]/g;
+    if (senha.value.match(numeral)) {
+        numero.classList.remove("invalido");
+        numero.classList.add("valido");
+    } else {
+        numero.classList.remove("valido");
+        numero.classList.add("invalido");
+    }
+    var letrasMinusculas = /[a-z]/g;
+    if (senha.value.match(letrasMinusculas)) {
+        minuscula.classList.remove("invalido");
+        minuscula.classList.add("valido");
+    } else {
+        minuscula.classList.remove("valido");
+        minuscula.classList.add("invalido");
+    }
+    var letraMaiuscula = /[A-Z]/g;
+    if (senha.value.match(letraMaiuscula)) {
+        maiuscula.classList.remove("invalido");
+        maiuscula.classList.add("valido");
+    } else {
+        maiuscula.classList.remove("valido");
+        maiuscula.classList.add("invalido");
+    }
+    var especial = /[!|@|#|$|%|^|&|*|(|)|-|_]/;
+    if (senha.value.match(especial)) {
+        caractere.classList.remove("invalido");
+        caractere.classList.add("valido");
+    } else {
+        caractere.classList.remove("valido");
+        caractere.classList.add("invalido");
+    }
+
+}
